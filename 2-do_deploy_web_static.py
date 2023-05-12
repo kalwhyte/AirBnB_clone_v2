c5c4c9ca2b45eb685b422cad27783bf23d2ecfa7
kalwhyte <superkalel55@gmail.com>
2023-05-12 14:46:17 +0000

    Fabric script that distributes an archive to my servers using the function do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,8 +13,8 @@ env.user = 'ubuntu'
 env.identity = '~/.ssh/school'
 env.password = None
 
-run("rm -rf /data/web_static/releases/web_static_20230506221613/images")
-run("rm -rf /data/web_static/releases/web_static_20230506221613/styles")
+#run("rm -rf /data/web_static/releases/web_static_20230506221613/images")
+#run("rm -rf /data/web_static/releases/web_static_20230506221613/styles")
 
 def do_deploy(archive_path):
     """
@@ -28,12 +28,12 @@ def do_deploy(archive_path):
         path_no_ext = "/data/web_static/releases/" + no_ext
 
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(path_no_ext))
-        run("tar -xzf /tmp/{} -C {}/".format(archive_name, path_no_ext))
-        run("rm /tmp/{}".format(archive_name))
-        run("mv {}/web_static/* {}/".format(path_no_ext, path_no_ext))
-        run("rm -rf {}/web_static".format(path_no_ext))
-        run("rm -rf /data/web_static/current")
+        sudo("mkdir -p {}".format(path_no_ext))
+        sudo("tar -xzf /tmp/{} -C {}/".format(archive_name, path_no_ext))
+        sudo("rm /tmp/{}".format(archive_name))
+        sudo("mv {}/web_static/* {}/".format(path_no_ext, path_no_ext))
+        sudo("rm -rf {}/web_static".format(path_no_ext))
+        sudo("rm -rf /data/web_static/current")
         sudo("ln -s {}/ /data/web_static/current".format(path_no_ext))
         sudo("chmod -R 755 {}".format(path_no_ext))
 
